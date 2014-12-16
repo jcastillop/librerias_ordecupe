2bc1210ad9f618fcc8d96e71d6a3c6a3fdd89943
cutti1990 <cutti1990@gmail.com>
2014-12-16 07:50:43 -0500

    permisos para el acceso

--- conexiones/class_rol.php
+++ conexiones/class_rol.php
@@ -35,6 +35,34 @@ class rol
 		}
 			return $this->rol;
 	}
+	
+		public function get_combo_rol_empresa_id($id)
+	{
+	echo 	$sql="
+		select cod_empresa,e.var_nom_emp,''dato from (
+select (int_cod_emp)cod_empresa from T_empresa where not int_cod_emp in (  select int_cod_emp from T_permisos 
+where int_cod_usu=".$id.")
+
+) as em inner join T_empresa e on e.int_cod_emp=em.cod_empresa
+
+union all 
+
+select p.int_cod_emp,e.var_nom_emp,(1)dato from T_permisos p 
+INNER join T_empresa e on e.int_cod_emp=p.int_cod_emp
+where p.int_cod_usu=".$id."
+		
+		";
+		
+		$res=mysql_query($sql,Conectar::con());
+		
+		while ($reg=mysql_fetch_assoc($res))
+		{
+			$this->rol[]=$reg;
+		}
+			return $this->rol;
+	}
+	
+	
 
 	public function get_usuario_por_id($id)
 	{
@@ -58,5 +86,29 @@ class rol
 		}
 			return $this->rol;
 	}
+	
+	public function get_combo_empresa()
+	{
+		$sql="select int_cod_emp,var_nom_emp from T_empresa
+		 ";
+		$res=mysql_query($sql,Conectar::con());
+		while ($reg=mysql_fetch_assoc($res))
+		{
+			$this->rol[]=$reg;
+		}
+			return $this->rol;
+	}
+	
+	public function eliminar_permisos($id)
+	{
+		$sql="delete from T_permisos where int_cod_usu=$id";
+		$res=mysql_query($sql,Conectar::con());
+		
+		
+	
+	}
+	
+	
+	
 }
 ?>
