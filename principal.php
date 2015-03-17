99c56446746d1dce1b1504ffdab2a6cfbef66987
dlandechop <dlandecho@sbcc-peru.com>
2015-03-17 16:40:50 -0500

    Modificacion del Menu
    Diseño a Bootstrap - Menu, contenido.

--- principal.php
+++ principal.php
@@ -43,157 +43,143 @@ $_SESSION['id_empresa']=1;
 $_SESSION['id_empresa']=1;
  ?>
 <!DOCTYPE html>
-
 <html lang="es">
 <head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-<LINK REL="SHORTCUT ICON" HREF="img/ordecupe.ico">
-<link href="img/ordecupe.ico" type="image/x-icon" rel="shortcut icon" />
-    <meta charset="utf-8">
-    <title>SISTEMA WEB</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Your page description here">
-    <meta name="author" content="">
-    <!-- css -->
-    <link href="css/menu.css" rel="stylesheet">
-
+    <meta charset="UTF-8">
+    <title>ORDECUPE</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="paquetes/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="paquetes/bootstrap/css/bootstrap-theme.css" rel="stylesheet" />
+    <link href="paquetes/bootstrap/css/bootstrap-submenu.min.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="css/easyui.css"/>
+    <link rel="stylesheet" type="text/css" href="css/icon.css"/>
+    <link href="css/menunew.css" rel="stylesheet">
     <script type="text/javascript" src="paquetes/alertas/lib/alertify.js"></script>
-    <link rel="stylesheet" href="paquetes/alertas/themes/alertify.core.css" />
-    <link rel="stylesheet" href="paquetes/alertas/themes/alertify.default.css" />
+
 </head>
+<body>
+    <div class="container-full">
+        <header>
+            <nav class="navbar navbar-default navbar-inverse">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="buttom" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-1">
+                            <span class="sr-only">Menu</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <div class="navbar-brand">
+                            <img src="css/images/ordecupe_logo_final.png" width="60" height="30">
+                        </div>
+                    </div>
 
-<!-- start header -->
-<header>	
+                    <div class="collapse navbar-collapse " id="navbar-1">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="dropdown"><!--MANTENIMIENTO-->
+                                <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button">MANTENIMIENTO <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="" onClick="addTab('Usuario','vista/mantenimiento/usuario/vista_usuario.php')">USUARIO</a></li>
+                                    <li><a class="" onClick="addTab('Cliente','vista/mantenimiento/cliente/vista_cliente.php')">CLIENTES</a></li>
+                                    <li><a class="" onClick="addTab('Proveedor','vista/mantenimiento/proveedor/vista_proveedor.php')">PROVEEDOR</a></li>
+                                    <li><a class="" onClick="addTab('Titulos','vista/mantenimiento/titulos/vista_titulos.php')">TITULOS</a></li>
+                                    <li><a class="" onClick="addTab('Sucursales','vista/mantenimiento/sucursales/vista_sucursales.php')">SUCURSALES</a></li>
+                                    <li><a class="" onClick="addTab('Editoriales','vista/mantenimiento/editorial/vista_editorial.php')">EDITORIALES</a></li>
+                                    <li><a class="" onClick="addTab('Tipo de Cambio','vista/mantenimiento/tipo_cambio/vista_tipocambio.php')">TIPO DE CAMBIO</a></li>
+                                    <li><a class="" onClick="addTab('Generos','vista/mantenimiento/genero/vista_generos.php')">GENEROS</a></li>
+                                    <li><a class="" onClick="addTab('Series','vista/mantenimiento/serie/vista_serie.php')">SERIES</a></li>
+                                </ul>
+                            </li><!--MANTENIMIENTO-->
 
-<div class="container">
-        
-<div class="navbar navbar-static-top">
-            <img src="css/images/ordecupe_logo_final.png" width="60" height="30">
-<div class="navigation">
-<nav>
-<ul class="nav topnav bold" id="navegador_tabs">
-<li class="dropdown active">
-<a >MANTENIMIENTO <i class="icon-angle-down"></i></a>
-<ul class="dropdown-menu bold">
-<li><a class="" onClick="addTab('Usuario','vista/mantenimiento/usuario/vista_usuario.php')">USUARIO</a></li>
-<li><a class="" onClick="addTab('Cliente','vista/mantenimiento/cliente/vista_cliente.php')">CLIENTES</a></li>
-<li><a class="" onClick="addTab('Proveedor','vista/mantenimiento/proveedor/vista_proveedor.php')">PROVEEDOR</a></li>
-<li><a class="" onClick="addTab('Titulos','vista/mantenimiento/titulos/vista_titulos.php')">TITULOS</a></li>
-<li><a class="" onClick="addTab('Sucursales','vista/mantenimiento/sucursales/vista_sucursales.php')">SUCURSALES</a></li>
-<li><a class="" onClick="addTab('Editoriales','vista/mantenimiento/editorial/vista_editorial.php')">EDITORIALES</a></li>
-                                    <!--<li><a class="" onClick="addTab('Monedas','vista/mantenimiento/Monedas/vista_monedas.php')">MONEDAS</a></li>-->
-<li><a class="" onClick="addTab('Tipo de Cambio','vista/mantenimiento/tipo_cambio/vista_tipocambio.php')">TIPO DE CAMBIO</a></li>
-<li><a class="" onClick="addTab('Generos','vista/mantenimiento/genero/vista_generos.php')">GENEROS</a></li>	
-<li><a class="" onClick="addTab('Series','vista/mantenimiento/serie/vista_serie.php')">SERIES</a></li>
-</ul>
-</li>
-<li class="dropdown">
-<a >ALMACEN <i class="icon-angle-down"></i></a>
-<ul class="dropdown-menu bold">
-<li><a class="" onClick="addTab('Transacciones','vista/almacen/registro/index.php')">REGISTRO</a></li>
-<li><a class="" onClick="addTab('Histórico transacciones','vista/almacen/consulta/vista_guia.php')">HISTÓRICO</a></li>
-              </ul>
-</li>
-                        <li class="dropdown">
-<a>VENTAS <i class="icon-angle-down"></i></a>
-<ul class="dropdown-menu bold">
-<li><a onClick="addTab('Registro Ventas','vista/ventas/registro/index.php')">REGISTRO</a></li>
-<li><a onClick="addTab('Histórico Ventas','vista/ventas/consulta/vista_ventas.php')">HISTÓRICO</a></li>
+                            <li class="dropdown"><!--ALMACEN-->
+                                <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button">ALMACEN <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="" onClick="addTab('Transacciones','vista/almacen/registro/index.php')">REGISTRO</a></li>
+                                    <li><a class="" onClick="addTab('Histórico transacciones','vista/almacen/consulta/vista_guia.php')">HISTORICO</a></li>
+                                </ul>
+                            </li><!--ALMACEN-->
 
-</ul>
-</li>
-<li class="dropdown">
-<a >COMPRAS<i class="icon-angle-down"></i></a>
-<ul class="dropdown-menu bold">
-<li><a onClick="addTab('Registro Compras','vista/compras/registro/index.php')">REGISTRO</a></li>
-<li><a onClick="addTab('Histórico Compras','vista/compras/consulta/vista_compras.php')">HISTÓRICO</a></li>
-</ul>
-</li>                           
-                            <li class="dropdown">
-<a>REPORTES<i class="icon-angle-down"></i></a>
-<ul class="dropdown-menu bold">
-<li class="dropdown">
-                                    <a>ALMACÉN<i class="icon-angle-down"></i></a>
-                                        <ul >
-                                        <li><a onClick="addTab('Stock General','vista/reportes/almacen/stock/index.php')">Stock General</a></li>
-                                                                                                         
+                            <li class="dropdown"><!--VENTAS-->
+                                <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button">VENTAS <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a onClick="addTab('Registro Ventas','vista/ventas/registro/index.php')">REGISTRO</a></li>
+                                    <li><a onClick="addTab('Histórico Ventas','vista/ventas/consulta/vista_ventas.php')">HISTORICO</a></li>
+                                </ul>
+                            </li><!--VENTAS-->
+
+                            <li class="dropdown"><!--COMPRAS-->
+                                <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button">COMPRAS <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a onClick="addTab('Registro Compras','vista/compras/registro/index.php')">REGISTRO</a></li>
+                                    <li><a onClick="addTab('Histórico Compras','vista/compras/consulta/vista_compras.php')">HISTORICO</a></li>
+                                </ul>
+                            </li><!--COMPRAS-->
+
+                            <li class="dropdown "><!--REPORTES-->
+                                <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button">REPORTES <span class="caret"></span></a>
+                                <ul class="dropdown-menu dropdown-menu-right" role="menu">
+                                    <li class="dropdown-submenu">
+                                        <a href="#">ALMACEN</a>
+                                        <ul class="dropdown-menu" role="menu">
+                                            <li><a onClick="addTab('Stock General','vista/reportes/almacen/stock/index.php')">Stock General</a></li>
                                         </ul>
                                     </li>
 
-<li class="dropdown">
-                                    <a>VENTAS<i class="icon-angle-down"></i></a>
-                                        <ul >
-                                        <li><a onClick="addTab('Reporte Ventas','vista/reportes/ventas/reporte_ventas.php')">Record Ventas</a></li>
-                                                                            
+                                    <li class="dropdown-submenu">
+                                        <a href="#">VENTAS</a>
+                                        <ul class="dropdown-menu" role="menu">
+                                            <li><a onClick="addTab('Reporte Ventas','vista/reportes/ventas/reporte_ventas.php')">Record de Ventas</a></li>
                                         </ul>
                                     </li>
-                                    
-                                    <li class="dropdown">
-                                    <a>COMPRAS<i class="icon-angle-down"></i></a>
-                                        <ul >
-                                        <li><a onClick="addTab('Reporte Compras','vista/reportes/compras/reporte_titulos.php')">Record Compras</a></li>
-                                                                            
-                                        </ul>
-                                    </li>	
-                                    <li class="dropdown">
-                                    <a>CONTABILIDAD<i class="icon-angle-down"></i></a>
-                                        <ul >
-                                        <li><a onClick="addTab('Registro de Ventas e Ingresos','vista/reportes/contabilidad/ventas_ingresos/index.php')">Registro de Ventas e Ingresos</a></li>
-                                        <li><a onClick="addTab('Registro de Compras','vista/reportes/contabilidad/compras_registro/index.php')">Registro de Compras</a></li>                                    
-                                        </ul>
-                                    </li>	                                   	
-</ul>
-</li>
-<li class="dropdown">
-<a href="#">SALIR <i class="icon-angle-down"></i></a>
-       <ul class="dropdown-menu bold">
-<li><a href="conexiones/logout.php">CERRAR SESION</a></li>
-</ul>
-</li>
-</ul>
-</nav>
-</div>
-<!-- end navigation -->
-</div>
-</div>
-</div>
-</div>
-</header>	
-<!-- end header -->
 
-    <!-- javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="js/jquery.js"></script>
-<script src="js/custom.js"></script>
+                                    <li class="dropdown-submenu">
+                                        <a href="#">COMPRAS</a>
+                                        <ul class="dropdown-menu" role="menu">
+                                            <li><a onClick="addTab('Reporte Compras','vista/reportes/compras/reporte_titulos.php')">Record de Compras</a></li>
+                                        </ul>
+                                    </li>
+
+                                        <li class="dropdown-submenu">
+                                        <a href="#">COONTABILIDAD</a>
+                                        <ul class="dropdown-menu" role="menu">
+                                            <li><a onClick="addTab('Registro de Ventas e Ingresos','vista/reportes/contabilidad/ventas_ingresos/index.php')">Registro de Ventas e Ingresos</a></li>
+                                            <li><a onClick="addTab('Registro de Compras','vista/reportes/contabilidad/compras_registro/index.php')">Registro de Compras</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li><!--REPORTES-->
+                            <li><!--CERRAR SESION-->
+                                <a href="conexiones/logout.php">CERRAR SESION</a>
+                            </li><!--COMPRAS-->
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    </div>
+    <div id="tt" class="col-dm-12 easyui-tabs" style="margin-top:-20px;" >
+        <div align="center" title="Inicio">
+            <p style="padding:80px;">
+            <img class="img-responsive" src="css/images/ordecupe_logo.png">
+            <p style="padding:60px;">
+        </div>
+    </div>
+
+<script src="js/jquery-vu.js"></script>
+<script type="text/javascript" src="http://www.jeasyui.com/easyui/jquery.easyui.min.js"></script>
+<script src="paquetes/bootstrap/js/bootstrap.min.js"></script>
+<script src="paquetes/bootstrap/js/bootstrap-submenu.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/easyui.css">
+<link rel="stylesheet" type="text/css" href="css/icon.css">
 <script src="js/funciones_principal.js"></script>
 
 
-
-<link rel="stylesheet" type="text/css" href="css/easyui.css">
-<link rel="stylesheet" type="text/css" href="css/icon.css">
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.4.min.js"></script>
-<script type="text/javascript" src="http://www.jeasyui.com/easyui/jquery.easyui.min.js"></script>
-
-    
-  <body>
-   <div id="tt" class="easyui-tabs" >
-<div align="center" title="INICIO">
-        <p style="padding:80px;">
-        <img src="css/images/ordecupe_logo.png">
-        <p style="padding:60px;">
-        
-</div>
-
-</div>  
- 
+<script>
+$(document).ready(function(){
+    $('.dropdown-submenu>a').submenupicker();
+});
+</script>
 
 </body>
-
-
-
-
-
-
-
 </html>
